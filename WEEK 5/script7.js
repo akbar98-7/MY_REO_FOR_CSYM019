0e0e45a66631c97ebf9817badb3b59c31286198a
Akber mirza <akbarmirza29@gmail.com>
2023-03-10 13:19:58 +0000

    Week 5 commits

---
+++ WEEK 5/script7.js
@@ -0,0 +1,12 @@
+function clickFunction() {
+    var element = document.getElementById('myinput');
+    var div = document.getElementById('result');
+    div.firstChild.nodeValue = element.value;
+   }
+
+   function myLoadFunction() {
+    var element = document.getElementById('myinput');
+    element.addEventListener('keyup', clickFunction);
+   }
+   
+   document.addEventListener('DOMContentLoaded', myLoadFunction);
